506fd5e85aca67a6e9568f9c142457992985ead5
Jeffry Gonzalez <jeff@hypertheory.com>
2025-01-28 10:35:02 -0500

    basic routing for demos

---
+++ src/app/demos/components/basic-signals.component.ts
@@ -0,0 +1,10 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-signals',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [],
+  template: ` <p>basic signals example</p> `,
+  styles: ``,
+})
+export class BasicSignalsComponent {}
