f879eb57b43a5452270c19b56b800edbc6e00281
Jeffry Gonzalez <jeff@hypertheory.com>
2025-01-30 11:39:06 -0500

    form kinda ready

--- src/app/resources/pages/create.component.ts
+++ src/app/resources/pages/create.component.ts
@@ -1,10 +1,84 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-create',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [],
-  template: ` <p>Add a New News Item Here (coming soon!)</p> `,
+  imports: [ReactiveFormsModule],
+  template: `
+    <form class="w-1/3" [formGroup]="form" (ngSubmit)="addThisThing()">
+      <div class="form-control">
+        <label for="title" class="label"
+          >Title
+          <input
+            formControlName="title"
+            type="text"
+            class="input input-bordered"
+            name="title"
+          />
+        </label>
+        @let titleControl = form.controls.title;
+        @if (
+          titleControl.errors && (titleControl.touched || titleControl.dirty)
+        ) {
+          <div class="alert alert-warning">
+            @if (titleControl.hasError('required')) {
+              <p>This is required</p>
+            }
+            @if (titleControl.hasError('maxlength')) {
+              <p>This is too darned long.</p>
+            }
+          </div>
+        }
+      </div>
+      <div class="form-control">
+        <label for="link" class="label"
+          >Link
+          <input
+            formControlName="link"
+            type="url"
+            class="input input-bordered"
+            name="link"
+          />
+        </label>
+      </div>
+      <div class="form-control">
+        <label for="description" class="label"
+          >Description
+          <textarea
+            formControlName="description"
+            type="text"
+            class="input input-bordered"
+            name="title"
+          ></textarea>
+        </label>
+      </div>
+      <button type="submit" class="btn btn-primary">Add News Item</button>
+    </form>
+  `,
   styles: ``,
 })
-export class CreateComponent {}
+export class CreateComponent {
+  form = new FormGroup({
+    title: new FormControl('', {
+      nonNullable: true,
+      validators: [Validators.required, Validators.maxLength(100)],
+    }),
+    link: new FormControl('', { nonNullable: true, validators: [] }),
+    description: new FormControl('', { nonNullable: true }),
+  });
+
+  addThisThing() {
+    // send it to the service, or store or whatever, if it is valid
+    if (this.form.valid) {
+      console.log(this.form.value);
+    } else {
+      console.log('has errors, bro.');
+    }
+  }
+}
